fe0cf513e29767affc68a6f9dc83ad0116bc698e
Arken <Arken.Sarsenov@outlook.com>
2025-05-06 13:12:03 +0500

    Firebase was connected to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:build_launch_monetize_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  //* firebase setup
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  //* run app
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,6 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(),
     );
   }
